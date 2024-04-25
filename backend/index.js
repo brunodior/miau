3af98a5ece8a9d3d9e4745a14aa7e05897311ed0
bruno dior <bruno.araujo.128@ufrn.edu.br>
2024-04-25 17:39:44 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors') //ajuda a fazer requisicao para o mesmo dominio
+
+const app = express()
+
+// config JSON response
+app.use(express.json())
+
+// solve CORS
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+
+// Public folder for images
+app.use(express.static('public'))
+
+
+// Routes
+const UserRoutes = require('./routes/UserRoutes')
+const PetRoutes = require('./routes/PetRoutes')
+
+
+app.use('/users', UserRoutes)
+app.use('/pets', PetRoutes)
+
+app.listen(5000)
